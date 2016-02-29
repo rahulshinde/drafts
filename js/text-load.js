3a5fd3ca0b584d4d1e62339d53b64a3398746b24
Rahul Subhash Shinde <rshinde@mica.edu>
2016-02-29 18:40:35 -0500

    editing tabs for intro and afterwards

--- js/text-load.js
+++ js/text-load.js
@@ -4,12 +4,14 @@ $(document).ready( function() {
 
     $(".intro").on("click", function() {
         $(".intro-text").animate({left: "60px"}, 750 );
+        $("#click1").animate({left: "48%"}, 750 );
         // $("#nav-open").fadeToggle("fast", function() {});
         // $(".mobile-nav").slideToggle("fast", function() {});       
     });
 
     $("#click1").on("click", function() {
         $(".intro-text").animate({left: "-1000px"}, 750 );
+        $("#click1").animate({left: "-500px"}, 750 );
         // $("#nav-open").fadeToggle("fast", function() {});
         // $(".mobile-nav").slideToggle("fast", function() {});       
     });
@@ -18,12 +20,14 @@ $(document).ready( function() {
 
     $(".afterwards").on("click", function() {
         $(".afterwards-text").animate({right: "60px"}, 750 );
+        $("#click2").animate({right: "48%"}, 750 );
         // $("#nav-open").fadeToggle("fast", function() {});
         // $(".mobile-nav").slideToggle("fast", function() {});       
     });
 
     $("#click2").on("click", function() {
         $(".afterwards-text").animate({right: "-1000px"}, 750 );
+        $("#click2").animate({right: "-500px"}, 750 );
         // $("#nav-open").fadeToggle("fast", function() {});
         // $(".mobile-nav").slideToggle("fast", function() {});       
     });
