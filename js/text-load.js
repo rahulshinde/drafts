a9ecd3ed0714ca6b62b61c39ee67bf3bcbe8a1dc
Rahul Subhash Shinde <rshinde@mica.edu>
2016-03-11 16:34:29 -0500

    new text loading

--- js/text-load.js
+++ js/text-load.js
@@ -1,26 +1,17 @@
 $(document).ready( function() {
 
-    var div1 = "60px";
-    var click = "575px";
-
     //intro
 
     $(".intro").on("click", function() {
         $(".intro-text").animate({left: "60px"}, 750 );
         $("#click1").animate({left: "575px"}, 750 );
+        $(".intro-close").toggle();
+    });
 
-        if (div1 = "60px") {
-            div1 = "-1000px";
-            click = "-500px";
-        }
-
-        else if (div1 = "-1000px") {
-            div1 = "60px";
-            click = "575px";
-        }  
-
-        console.log(div1);
-        console.log(click); 
+    $(".intro-close").on("click", function() {
+        $(".intro-text").animate({left: "-1000px"}, 750 );
+        $("#click1").animate({left: "-500px"}, 750 );
+        $(".intro-close").toggle();       
     });
 
     $("#click1").on("click", function() {
@@ -32,7 +23,14 @@ $(document).ready( function() {
 
     $(".afterwards").on("click", function() {
         $(".afterwards-text").animate({right: "60px"}, 750 );
-        $("#click2").animate({right: "575px"}, 750 );      
+        $("#click2").animate({right: "575px"}, 750 );
+        $(".afterwards-close").toggle();      
+    });
+
+    $(".afterwards-close").on("click", function() {
+        $(".afterwards-text").animate({right: "-1000px"}, 750 );
+        $("#click2").animate({right: "-500px"}, 750 );
+        $(".afterwards-close").toggle();   
     });
 
     $("#click2").on("click", function() {
