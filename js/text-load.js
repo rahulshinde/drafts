472c106781c51783af722336737c91947a55e150
Rahul Subhash Shinde <rshinde@mica.edu>
2016-03-11 14:04:23 -0500

    new link to new website, will update once page build is complete

--- js/text-load.js
+++ js/text-load.js
@@ -1,35 +1,43 @@
 $(document).ready( function() {
 
+    var div1 = "60px";
+    var click = "575px";
+
     //intro
 
     $(".intro").on("click", function() {
         $(".intro-text").animate({left: "60px"}, 750 );
         $("#click1").animate({left: "575px"}, 750 );
-        // $("#nav-open").fadeToggle("fast", function() {});
-        // $(".mobile-nav").slideToggle("fast", function() {});       
+
+        if (div1 = "60px") {
+            div1 = "-1000px";
+            click = "-500px";
+        }
+
+        else if (div1 = "-1000px") {
+            div1 = "60px";
+            click = "575px";
+        }  
+
+        console.log(div1);
+        console.log(click); 
     });
 
     $("#click1").on("click", function() {
         $(".intro-text").animate({left: "-1000px"}, 750 );
-        $("#click1").animate({left: "-500px"}, 750 );
-        // $("#nav-open").fadeToggle("fast", function() {});
-        // $(".mobile-nav").slideToggle("fast", function() {});       
+        $("#click1").animate({left: "-500px"}, 750 );       
     });
 
     //afterwards
 
     $(".afterwards").on("click", function() {
         $(".afterwards-text").animate({right: "60px"}, 750 );
-        $("#click2").animate({right: "575px"}, 750 );
-        // $("#nav-open").fadeToggle("fast", function() {});
-        // $(".mobile-nav").slideToggle("fast", function() {});       
+        $("#click2").animate({right: "575px"}, 750 );      
     });
 
     $("#click2").on("click", function() {
         $(".afterwards-text").animate({right: "-1000px"}, 750 );
-        $("#click2").animate({right: "-500px"}, 750 );
-        // $("#nav-open").fadeToggle("fast", function() {});
-        // $(".mobile-nav").slideToggle("fast", function() {});       
+        $("#click2").animate({right: "-500px"}, 750 );    
     });
 
 });
